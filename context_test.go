98758c606e5dba4633c52831c834c5bdb6beea57
ilhamarfian <ilham.arfian@btpn.com>
2025-01-23 19:27:37 +0700

    context with value on parent and child context

--- context_test.go
+++ context_test.go
@@ -13,3 +13,29 @@ func TestContext(t *testing.T) {
 	todoContext := context.TODO()
 	fmt.Println(todoContext)
 }
+
+func TestWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+	contextG := context.WithValue(contextF, "g", "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	fmt.Println(contextF.Value("f")) // Output: F
+	fmt.Println(contextF.Value("c")) // Output: C
+	fmt.Println(contextF.Value("b")) // Output: nil, different parent A -> C -> F
+	fmt.Println(contextA.Value("b")) // Output: nil, parent cant invoke a child value
+}
